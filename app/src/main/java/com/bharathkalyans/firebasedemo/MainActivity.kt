b720c11685af5bd098d1bc11740aa433dc63f2f2
Bharath Kalyan S <sbharath35@gmail.com>
2021-06-16 13:07:59 +0530

    Initial commit

---
+++ app/src/main/java/com/bharathkalyans/firebasedemo/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.bharathkalyans.firebasedemo
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+
+class MainActivity : AppCompatActivity() {
+
+    private val personCollectionRef = Firebase.firestore.collection("persons")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        btnSaveToDatabase.setOnClickListener {
+            val firstName = firstName.text.toString()
+            val lastName = lastName.text.toString()
+            val age = age.text.toString().toInt()
+
+            val person = Person(firstName, lastName, age)
+
+            savePerson(person)
+        }
+    }
+
+    private fun savePerson(person: Person) = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            personCollectionRef.add(person).await()
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, "Successfully Saved Data", Toast.LENGTH_SHORT).show()
+            }
+        } catch (e: Exception) {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/bharathkalyans/firebasedemo/Person.kt
@@ -0,0 +1,7 @@
+package com.bharathkalyans.firebasedemo
+
+data class Person(
+    var firstName: String,
+    var lastName: String,
+    var age: Int
+)
